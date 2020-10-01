29651b3876f899f18ff747ef347dd240e7c02878
Maxim <adiantium@gmail.com>
2020-10-01 12:07:04 +0700

    fix navigation menu when screnn flips or then menu opens

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const checkboxMenu = document.getElementById("checkbox-menu"),
+  kvastLogo = document.querySelector(".header_logo"),
+  kvastTitle = document.querySelector(".hedaer_title");
 
+checkboxMenu.addEventListener('click', (e) => {
+  if (checkboxMenu.checked) {
+
+    kvastLogo.style.display = "none";
+    kvastTitle.style.display = "none";
+  } else {
+    kvastLogo.style.display = "block";
+    kvastTitle.style.display = "block";
+  }
+});
+
+window.onresize = (e) => {
+  checkboxMenu.checked = false;
+  kvastLogo.style.display = "block";
+  kvastTitle.style.display = "block";
+}
